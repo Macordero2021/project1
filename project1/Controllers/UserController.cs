341fa3a1f6c8563ef119291da5d34e0880eb0165
Mario Cordero <Mario Cordero@DESKTOP-B9TSVOR>
2023-07-02 20:28:07 -0600

    Se conecta a bse de datos para trabajar con los Roles

--- project1/Controllers/UserController.cs
+++ project1/Controllers/UserController.cs
@@ -1,10 +1,12 @@
-﻿using project1.Models.DAO;
+﻿using project1.Models;
+using project1.Models.DAO;
 using project1.Models.DTO;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Timers;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
 
 namespace project1.Controllers
@@ -13,10 +15,21 @@ namespace project1.Controllers
     {
         private UserDAO userRepository = new UserDAO();
 
+        private AuthenticationConfig _db = new AuthenticationConfig();
+
         // GET: User
         public ActionResult Index()
         {
+            
+            List<UserDTO> users = userRepository.ReadUsers();
+            //get an specific user(5)
+            UserDTO user = (from u in users where 5 == u.Id select u).First();
+            // get role
+            RolesUsuario ru = _db.RolesUsuario.Where(x => x.IdUser == user.Id).FirstOrDefault();
+            //asing a session variable
+            Session["role"] = _db.Roles.Where(x => x.Id == ru.IdRole).FirstOrDefault().Description;
 
+            var roles = _db.Roles.ToList();
             return View(userRepository.ReadUsers());
         }
 
---
+++ project1/Models/AuthenticationConfig.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace project1.Models
+{
+    public partial class AuthenticationConfig : DbContext
+    {
+        public AuthenticationConfig()
+            : base("name=AuthenticationConfig")
+        {
+        }
+
+        public virtual DbSet<Roles> Roles { get; set; }
+        public virtual DbSet<RolesUsuario> RolesUsuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Roles>()
+                .Property(e => e.Description)
+                .IsUnicode(false);
+        }
+    }
+}
